33542212f443f32311afac401127e81ad982f9e9
GasinAn <Gasin185@163.com>
2023-07-02 18:46:53 +0800

    use for

---
+++ yao.f90
@@ -0,0 +1,40 @@
+program yao
+    use iso_fortran_env, only: dp => real64
+    implicit none
+    real(dp),parameter :: pi = acos(-1.0_dp)
+    real(dp) :: t, t_jd_j2000, EO
+    real(dp) :: a_icrs_ra, b_icrs_ra, c_icrs_ra, d_icrs_ra
+    real(dp) :: a_icrs_dec, b_icrs_dec, c_icrs_dec, d_icrs_dec
+    real(dp) :: a_cirs_ra, b_cirs_ra, c_cirs_ra, d_cirs_ra
+    real(dp) :: a_cirs_dec, b_cirs_dec, c_cirs_dec, d_cirs_dec
+    a_icrs_ra = 09.0_dp+27.0_dp/60.0_dp+35.24270/3600.0_dp
+    b_icrs_ra = 16.0_dp+29.0_dp/60.0_dp+24.45970/3600.0_dp
+    c_icrs_ra = 21.0_dp+31.0_dp/60.0_dp+33.5317148/3600.0_dp
+    d_icrs_ra = 03.0_dp+46.0_dp/60.0_dp+24.2/3600.0_dp
+    a_icrs_dec = -(08.0_dp+39.0_dp/60.0_dp+30.9583/3600.0_dp)
+    b_icrs_dec = -(26.0_dp+25.0_dp/60.0_dp+55.2094/3600.0_dp)
+    c_icrs_dec = -(05.0_dp+34.0_dp/60.0_dp+16.232006/3600.0_dp)
+    d_icrs_dec = +(24.0_dp+06.0_dp/60.0_dp+50.0/3600.0_dp)
+    a_icrs_ra = a_icrs_ra*(pi/12.0_dp)
+    b_icrs_ra = b_icrs_ra*(pi/12.0_dp)
+    c_icrs_ra = c_icrs_ra*(pi/12.0_dp)
+    d_icrs_ra = d_icrs_ra*(pi/12.0_dp)
+    a_icrs_dec = a_icrs_dec*(pi/180.0_dp)
+    b_icrs_dec = b_icrs_dec*(pi/180.0_dp)
+    c_icrs_dec = c_icrs_dec*(pi/180.0_dp)
+    d_icrs_dec = d_icrs_dec*(pi/180.0_dp)
+    t = 2000.0_dp
+    t_jd_j2000 = (t-2000.0_dp)*365.25_dp-0.5_dp
+    call iau_ATCI13(a_icrs_ra, a_icrs_dec, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, & 
+                    2451545_dp, t_jd_j2000, a_cirs_ra, a_cirs_dec, EO)
+    call iau_ATCI13(b_icrs_ra, b_icrs_dec, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, & 
+                    2451545_dp, t_jd_j2000, b_cirs_ra, b_cirs_dec, EO)
+    call iau_ATCI13(c_icrs_ra, c_icrs_dec, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, & 
+                    2451545_dp, t_jd_j2000, c_cirs_ra, c_cirs_dec, EO)
+    call iau_ATCI13(d_icrs_ra, d_icrs_dec, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, & 
+                    2451545_dp, t_jd_j2000, d_cirs_ra, d_cirs_dec, EO)
+    print *, a_icrs_ra, b_icrs_ra, c_icrs_ra, d_icrs_ra
+    print *, a_icrs_dec, b_icrs_dec, c_icrs_dec, d_icrs_dec
+    print *, a_cirs_ra, b_cirs_ra, c_cirs_ra, d_cirs_ra
+    print *, a_cirs_dec, b_cirs_dec, c_cirs_dec, d_cirs_dec
+end program yao
--- yao.py
+++ yao.py
@@ -1,8 +1,9 @@
-from astropy.units import hourangle, degree
+from math import pi
+from astropy.units import hourangle, degree, rad
 from astropy.time import Time
 from astropy.coordinates import SkyCoord
 
-t = -8000
+t = 2000
 t = Time(2451545+(t-2000)*365.25, scale='tt', format='jd')
 A = SkyCoord('09 27 35.24270 -08 39 30.9583', obstime=t, frame='gcrs',
              unit=(hourangle, degree), equinox='J2000')
@@ -12,4 +13,11 @@ C = SkyCoord('21 31 33.5317148 -05 34 16.232006', obstime=t,  frame='gcrs',
              unit=(hourangle, degree), equinox='J2000')
 D = SkyCoord('03 46 24.2 +24 06 50', obstime=t,  frame='gcrs',
              unit=(hourangle, degree), equinox='J2000')
-print(A.cirs,B.cirs,C.cirs,D.cirs)
+print(A.ra.to(rad),B.ra.to(rad),
+      C.ra.to(rad),D.ra.to(rad))
+print(A.dec.to(rad),B.dec.to(rad),
+      C.dec.to(rad),D.dec.to(rad))
+print(A.cirs.ra.to(rad),B.cirs.ra.to(rad),
+      C.cirs.ra.to(rad),D.cirs.ra.to(rad))
+print(A.cirs.dec.to(rad),B.cirs.dec.to(rad),
+      C.cirs.dec.to(rad),D.cirs.dec.to(rad))
